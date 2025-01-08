e9ab5bb5dadc6d7544045ae4a72b2d9243da64e2
tylerpederson <tyler.lee.pederson@gmail.com>
2025-01-07 21:47:19 -0800

    Add ship with turning

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import pygame
+from player import *
 from constants import * 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while(True):
-        screen.fill(pygame.Color(0, 0, 0))
-        pygame.display.flip()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.update(dt)
+            
+        screen.fill(pygame.Color(0, 0, 0))
+
+        player.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
